e0a5de9146e5f6dbd3a20029fcead0bcfdf5751c
Stevenson254 <odhiambosteven761@gmail.com>
2025-03-11 11:13:07 +0300

    first commit

---
+++ Intro_take2.py
@@ -0,0 +1,14 @@
+#integer (whole number)
+age = 30
+# float (decimal number)
+height = 7.8945
+# string (text)
+name = "Derrick"
+# boolean (true or false statement)
+student = True
+
+# Check Data Type
+print(type(age)) 
+print(type(height))
+print(type(name))
+print(type(student))
---
+++ TempaZ.py
@@ -0,0 +1,6 @@
+temperature = 24.9
+
+if temperature > 25:
+    print("It’s a hot day! ☀️")
+else:
+    print("It’s a cool day! ❄️")
